dec747b825267341f5271f4c054b582467e0d8f9
Fernando Odorico <fernando.odorico@geopagos.com>
2023-03-17 22:54:24 -0300

    Se re acomoda las cosas

---
+++ Repaso/Solution/EjercicioConsola01/Program.cs
@@ -0,0 +1,20 @@
+namespace EjercicioConsola01;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var registryNumber = 882831;
+        for (var i = 1; i <= Math.Abs(registryNumber % 1000); i++)
+        {
+            var word = i % 3 == 0 && i % 5 == 0 ? $"{i} – FooBar" :
+                i % 3 == 0 ? $"{i} – Foo" :
+                i % 5 == 0 ? $"{i} – Bar" :
+                $"{i}";
+
+            Console.WriteLine(word);
+        }
+
+        Environment.Exit(1);
+    }
+}
